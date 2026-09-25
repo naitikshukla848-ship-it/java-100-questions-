e57e1bb9fb7c1083b3d606649a0aa44f7d69991f
naitikshukla848-ship-it <naitikshukla848@gmail.com>
2026-09-25 21:44:27 +0530

    Create Q060CopyConstructor.java

---
+++ 100-java-question-solutions/100 java question/Q060CopyConstructor.java
@@ -0,0 +1,49 @@
+public class Q060CopyConstructor {
+
+  // 60. CopyConstructor: Demonstrates the requested OOP concept.
+  static class Parent {
+
+    void action() {
+      System.out.println("Parent action");
+    }
+  }
+
+  static class Child extends Parent {
+
+    @Override
+    void action() {
+      System.out.println("Child override");
+    }
+  }
+
+  interface Feature {
+    default void demonstrate() {
+      System.out.println("Interface default method");
+    }
+  }
+
+  abstract static class Base {
+
+    abstract void work();
+
+    final void common() {
+      System.out.println("Final/concrete method");
+    }
+  }
+
+  static class Example extends Base implements Feature {
+
+    void work() {
+      System.out.println("Abstract method implemented");
+    }
+  }
+
+  public static void main(String[] args) {
+    Parent p = new Child();
+    p.action();
+    Example e = new Example();
+    e.common();
+    e.work();
+    e.demonstrate();
+  }
+}
